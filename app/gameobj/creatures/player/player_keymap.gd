be1b78d583af6c915c6c942bf1d8a8091c9376f5
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-10 10:31:27 -0300

    Player movement
    Created player movement mechanic for 8-directional movement in steps and with run multiplier

--- app/gameobj/creatures/player/player_keymap.gd
+++ app/gameobj/creatures/player/player_keymap.gd
@@ -1,4 +1,16 @@
-extends Node
+extends Node2D
+
+signal move(dir: float)
+signal start_running
+signal stop_running
 
 func _input(_event: InputEvent) -> void:
-	pass
+	if _event.is_action_pressed("attack"):
+		var mouse = get_local_mouse_position()
+		var angle = mouse.angle() #snappedf(mouse.angle(), PI/4) / (PI/4)
+		angle = round(8*(angle / (2*PI))) / 8 * 2*PI
+		move.emit(angle)
+	elif _event.is_action_pressed("run"):
+		start_running.emit()
+	elif _event.is_action_released("run"):
+		stop_running.emit()
