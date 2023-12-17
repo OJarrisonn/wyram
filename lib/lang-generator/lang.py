9b2607c3b95cf3931f4d7ffb57e141e67d822e08
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-17 01:47:30 -0300

    Refactored Language construction
    A language is made of word, which are made of sillables
    
    A sillable is a list of tokens, where each part can be a single token of a set of tokens (a weighted list of tokens)
    
    Each token has a preffix, a core and a suffix, each is a weighted list of strings

--- lib/lang-generator/lang.py
+++ lib/lang-generator/lang.py
@@ -2,46 +2,50 @@ from random import randint
 
 from nltk.probability import FreqDist, MLEProbDist
 
-class Vowel:
-    def __init__(self, v: str, modifiers={'': 1}) -> None:
-        self._me = v
-        self._modifiers = MLEProbDist(FreqDist(modifiers))
+class Token:
+    def __init__(self, core: dict[str, float], suffixes={'': 1}, prefixes={'':1}) -> None:
+        self._core = MLEProbDist(FreqDist(core))
+        self._suffixes = MLEProbDist(FreqDist(suffixes))
+        self._prefixes = MLEProbDist(FreqDist(prefixes))
     
-    def get(self) -> str:
-        out = self._me
-        out += self._modifiers.generate()
-        return out
-    
-    def __format__(self, __format_spec: str) -> str:
-        self._me
+    def get_token(self) -> tuple[str, str, str]:
+        return (
+            self._prefixes.generate(),
+            self._core.generate(),
+            self._suffixes.generate()
+        )
 
-class Consonant:
-    def __init__(self, c: str, modifiers={'': 1}) -> None:
-        self._me = c
-        self._modifiers = MLEProbDist(FreqDist(modifiers))
-    
-    def get(self) -> str:
-        out = self._me
-        out += self._modifiers.generate()
-        return out
 
-    def __format__(self, __format_spec: str) -> str:
-        self._me
+class TokenSet:
+    def __init__(self, tokens: dict[Token, float]) -> None:
+        self._tokens = MLEProbDist(FreqDist(tokens))
+    
+    def get_token(self) -> tuple[str, str, str]:
+        return self._tokens.generate().get_token()
+
+class Sillable:
+    def __init__(self, tokens: list):
+        self._rule = tokens
+    
+    def get_sillable(self) -> list[tuple[str, str, str]]:
+        value = []
+        
+        for tk in self._rule:
+            value.append(tk.get_token())
+        return value
 
 class Language:
-    def __init__(self, vowels, consonants, default_sillables=4) -> None:
-        self._vowels = MLEProbDist(FreqDist(vowels))
-        self._consonants = MLEProbDist(FreqDist(consonants))
-        self._default_sillables = default_sillables
+    def __init__(self, sillables: dict[Sillable, float], default_sillable_count=1) -> None:
+        self._sillables = MLEProbDist(FreqDist(sillables))
+        self._default_sillable_count = default_sillable_count
     
     def generate_word(self, sillables=0) -> list[str]:
         if sillables == 0:
-            sillables = randint(1, self._default_sillables)
+            sillables = randint(1, self._default_sillable_count)
         
         word = []
         
         for i in range(sillables):
-            word.append(self._consonants.generate().get())
-            word.append(self._vowels.generate().get())
+            word.append(self._sillables.generate().get_sillable())
 
         return word
