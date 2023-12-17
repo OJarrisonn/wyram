e963de0d8342a7fc69f23fe33e855f66410d086b
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-17 01:51:51 -0300

    Changed test to just generate a word

--- lib/lang-generator/main.py
+++ lib/lang-generator/main.py
@@ -6,24 +6,4 @@ from pprint import pprint
 
 pr = Primitive()
 
-text = ""
-words = []
-
-for i in range(100):
-    words.append(pr.generate_word())
-    text += ''.join(words[-1])
-    
-    point = randint(0, 19)
-    
-    if point <= 3:
-        text += ', '
-    elif point <= 5:
-        text += '. '
-    elif point == 6:
-        text += '.\n'
-    else:
-        text += ' '
-    
-
-print(text)
-pprint(words)
+print(pr.generate_word())
