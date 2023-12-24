683bc2bead023d4acde20acc5ad9a9de40239d3f
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-24 15:03:38 -0300

    Added event catching for player interactions

--- app/gameobj/living/player/player_input.gd
+++ app/gameobj/living/player/player_input.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 signal start_running
 signal stop_running
+signal attack
+signal interact
 
 func _input(_event: InputEvent) -> void:
 	if _event.is_action_pressed("run"):
 		start_running.emit()
 	elif _event.is_action_released("run"):
 		stop_running.emit()
+	elif _event.is_action_pressed("attack"):
+		attack.emit()
+	elif _event.is_action_pressed("interact"):
+		interact.emit()
