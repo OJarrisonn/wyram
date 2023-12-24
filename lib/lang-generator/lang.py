ef2f7ca39f632051dab2c8bb6d2699d265e35538
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-24 14:52:26 -0300

    Full refactor of the concept of lang
    A language is made of words, which are made of syllables. A syllable has an onset and a rhyme.
    
    To create a syllable we just choose an onset and a rhyme from the options

--- lib/lang-generator/lang.py
+++ lib/lang-generator/lang.py
@@ -1,51 +1,30 @@
-from random import randint
+from random import choice
 
-from nltk.probability import FreqDist, MLEProbDist
+class Syllable:
+    def __init__(self) -> None:
+        self.__vowels = [
+            'a', 'am', 'aq', 'as', 'al', 'ar',
+            'e', 'en', 'ek', 'ec', 'ew', 'eh',
+            'i', 'in', 'ik', 'ic', 'iw', 'ih',
+            'ae', 'aen', 'aek', 'aec', 'aew', 'aeh',
+            'oe', 'oen', 'oek', 'oec', 'oew', 'oeh',
+            'o', 'om', 'oq', 'os', 'ol', 'or',
+            'u', 'um', 'uq', 'us', 'ur',
+        ]
 
-class Token:
-    def __init__(self, core: dict[str, float], suffixes={'': 1}, prefixes={'':1}) -> None:
-        self._core = MLEProbDist(FreqDist(core))
-        self._suffixes = MLEProbDist(FreqDist(suffixes))
-        self._prefixes = MLEProbDist(FreqDist(prefixes))
+        self.__consonants = [
+            'b', 'bh', 
+            'd', 'dx',
+            'f', 
+            'g', 'gh', 
+            'l', 'm', 'n',
+            'p', 'ph', 
+            'q', 
+            'r', 'rx', 
+            's', 'sx', 
+            't', 'th', 'tx',
+            'v', 'z'
+        ]
     
-    def get_token(self) -> tuple[str, str, str]:
-        return (
-            self._prefixes.generate(),
-            self._core.generate(),
-            self._suffixes.generate()
-        )
-
-
-class TokenSet:
-    def __init__(self, tokens: dict[Token, float]) -> None:
-        self._tokens = MLEProbDist(FreqDist(tokens))
-    
-    def get_token(self) -> tuple[str, str, str]:
-        return self._tokens.generate().get_token()
-
-class Sillable:
-    def __init__(self, tokens: list):
-        self._rule = tokens
-    
-    def get_sillable(self) -> list[tuple[str, str, str]]:
-        value = []
-        
-        for tk in self._rule:
-            value.append(tk.get_token())
-        return value
-
-class Language:
-    def __init__(self, sillables: dict[Sillable, float], default_sillable_count=1) -> None:
-        self._sillables = MLEProbDist(FreqDist(sillables))
-        self._default_sillable_count = default_sillable_count
-    
-    def generate_word(self, sillables=0) -> list[str]:
-        if sillables == 0:
-            sillables = randint(1, self._default_sillable_count)
-        
-        word = []
-        
-        for i in range(sillables):
-            word.append(self._sillables.generate().get_sillable())
-
-        return word
+    def generate(self) -> list[str]:
+        return [choice(self.__consonants), choice(self.__vowels)]
