84ae910c7ca1d4a6d72c15e03dfa8db5ea2cc40a
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-24 14:55:28 -0300

    Our main file now contains a function to generate a word of a given count of syllables and a function to generate a text of a given count of words

--- lib/lang-generator/main.py
+++ lib/lang-generator/main.py
@@ -1,9 +1,27 @@
-from primitive import Primitive
-
+from lang import Syllable
 from random import randint
 
-from pprint import pprint
 
-pr = Primitive()
+def generate_word(syllable_count):
+    s = Syllable()
 
-print(pr.generate_word())
+    w = ""
+    for _ in range(syllable_count):
+        for sp in s.generate():
+            w += sp
+    return w
+
+def generate_text(word_count, max_syllable_count):
+    s = Syllable()
+    
+    text = ""
+    wc = word_count
+    wl = max_syllable_count
+
+    for _ in range(wc):
+        sc = randint(1, wl) # Syllable count
+        w = generate_word(sc) # current word
+
+        w += " "
+        text += w
+    return text
