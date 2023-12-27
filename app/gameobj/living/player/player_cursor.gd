59446bfa8b76b7cda753ee21e1d79141ff453435
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-26 22:04:38 -0300

    Function to update the cursor position
    This function is called by the player to update its cursor position every frame
    
    This function also limits the position of the cursor to no bee to far away from the player
    
    TODO: Cast a ray from the center to the click point and place the cursor in the
    Closest hit body

--- app/gameobj/living/player/player_cursor.gd
+++ app/gameobj/living/player/player_cursor.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var cursor_range: float = 250.0
+
 var __attackable_targets = []
 var __interactible_targets = []
 
@@ -24,3 +26,25 @@ func _on_body_exited(body):
 		__attackable_targets.erase(body)
 	if body.is_in_group('interactible'):
 		__interactible_targets.erase(body)
+
+func update() -> void:
+	# The vector of how much has the mouse moved
+	var cursor_delta = get_parent().get_local_mouse_position()
+	
+	if cursor_delta.length() > cursor_range: # Ensures it is within the range
+		cursor_delta = cursor_delta.normalized() * cursor_range
+		
+	position = cursor_delta # Moves the cursor to the new position
+
+func get_attack_target() -> Node2D:
+	if __attackable_targets.size() > 0:
+		print("[PLCS]: Attacking " + str(__attackable_targets[0]))
+		return __attackable_targets[0]
+	return null
+	
+func get_interact_target() -> Node2D:
+	if __interactible_targets.size() > 0:
+		print("[PLCS]: Interacting with " + str(__interactible_targets[0]))
+		return __interactible_targets[0]
+	return null
+
