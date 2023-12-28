f2f93634396cd86f209ce62c7fe286ae9ca368f9
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-28 10:33:09 -0300

    The stack size is now part of the item definition

--- app/gameobj/logistic/inventory_item.gd
+++ app/gameobj/logistic/inventory_item.gd
@@ -1,9 +1,18 @@
 class_name InventoryItem
 
 var __id: String
+var __max_stack: int = 50
 
 func _init(id: String):
 	__id = id
 
 func get_id() -> String:
 	return __id
+
+func set_max_stack(value: int) -> InventoryItem:
+	__max_stack = value
+	
+	return self
+
+func get_max_stack() -> int:
+	return __max_stack
