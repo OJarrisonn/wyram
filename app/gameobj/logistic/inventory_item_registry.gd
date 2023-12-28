366caa9c5513721db2b6f9c2e4134c0665c23a9f
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-28 10:33:34 -0300

    Added a function to register new items, to get items from registry and to register the vanilla items

--- app/gameobj/logistic/inventory_item_registry.gd
+++ app/gameobj/logistic/inventory_item_registry.gd
@@ -1,2 +1,14 @@
-func Void() -> InventoryItem: return InventoryItem.new("w/void")
-func Wood() -> InventoryItem: return InventoryItem.new("w/wood")
+static var __registry = {}
+
+func register_all():
+	add_registry(InventoryItem.new("w::void"))
+	add_registry(InventoryItem.new("w::wood"))
+
+func add_registry(item: InventoryItem):
+	if __registry.has(item.get_id()):
+		return
+	
+	__registry[item.get_id()] = item
+
+func get_registry(id: String):
+	return __registry[id]
